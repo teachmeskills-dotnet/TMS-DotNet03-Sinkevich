e316feb5e4e6b221299c604bc4017b874d661d13
Julia Sinkevich <66641254+julsinkevich@users.noreply.github.com>
2021-01-14 23:49:53 +0300

    feat: update-database (erro: 1) "Failed executing DbCommand (2ms)" 2) "There is already an object named 'AspNetRoles' in the database."

--- src/FindHousingProject.Web/Startup.cs
+++ src/FindHousingProject.Web/Startup.cs
@@ -1,6 +1,10 @@
+using FindHousingProject.DAL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,11 +16,22 @@ namespace FindHousingProject.Web
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,6 +48,9 @@ namespace FindHousingProject.Web
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseAuthentication();    // подключение аутентификации
+            app.UseAuthorization();
 
             /* app.UseEndpoints(endpoints =>
              {
