d3c142baff9b9d880543e0d55ca313d0e6b77b54
Julia Sinkevich <66641254+julsinkevich@users.noreply.github.com>
2021-01-31 17:31:31 +0300

    feat: add structure mvc

--- src/FindHousingProject.Web/Controllers/HomeController.cs
+++ src/FindHousingProject.Web/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
-﻿using FindHousingProject.Web.ViewModels;
+﻿using Abp.Web.Mvc.Models;
+using FindHousingProject.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,8 +12,10 @@ namespace FindHousingProject.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
         private readonly SimpleViewModel _vm;
-        public HomeController()
+        public HomeController(ILogger<HomeController> logger)
         {
             var rnd = new Random();
 
@@ -18,6 +23,8 @@ namespace FindHousingProject.Web.Controllers
             {
                 Value = rnd.Next(minValue: 0, maxValue: 100)
             };
+            _logger = logger;
+
         }
         public IActionResult Index()
         {
@@ -28,6 +35,11 @@ namespace FindHousingProject.Web.Controllers
         public IActionResult Another()
         {
             return View(_vm);
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ViewModels.ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
